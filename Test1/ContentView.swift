a69962eb6df52d927b0781867c7be730bc300314
slack-pentest1 <slack-pentest1@Blueinfy.net>
2024-03-18 13:54:16 +0530

    ok

--- Test1/ContentView.swift
+++ Test1/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Test")
+            Text("Test2")
         }
         .padding()
     }
